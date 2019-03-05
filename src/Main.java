e5dd60d905f7b537d0648feea03aa6547ed81cfb
XY-ZHOU <731986251@qq.com>
2019-03-05 21:48:12 +0800

    achieve function of calculation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import main.java.model.Calculation;
+
 public class Main {
+    public static void main(String[] args) {
+        Calculation test = new Calculation(6, 3);
+        System.out.println(test.calculate((i, j) -> i + j));
+        System.out.println(test.calculate((i, j) -> i - j));
+        System.out.println(test.calculate((i, j) -> i * j));
+        System.out.println(test.calculate((i, j) -> i / j));
+    }
 }
