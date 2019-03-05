1a040cca6392e290a974d03bf3db8b6869595984
XY-ZHOU <731986251@qq.com>
2019-03-05 21:47:44 +0800

    achieve function of calculation

--- src/main/java/model/Calculation.java
+++ src/main/java/model/Calculation.java
@@ -1,4 +1,15 @@
 package main.java.model;
 
 public class Calculation {
+    private int i;
+    private int j;
+
+    public Calculation(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int calculate(MyFunctionalInterface res) {
+        return res.function(i, j);
+    }
 }
